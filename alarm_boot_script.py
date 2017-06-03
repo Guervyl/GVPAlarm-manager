615274085952b5cf00a39e93fe806d12176e6c48
Guerby Duval <duval.guerby@yahoo.fr>
2017-06-03 16:34:21 -0500

    Update alarm_boot_script.py

--- alarm_boot_script.py
+++ alarm_boot_script.py
@@ -4,10 +4,8 @@ import set_rtc_alarm_wake
 
 # Do action when an alarm got executed
 def executeAlarm(alarmTime):
-    print("exec ")
-    os.system("vlc '/media/usb1/Guervyl_backup/Download/to add/new'")
+    print("executed")
     
 # Do action for new alarm
 def alarmInit(alarmTime):
-    set_rtc_alarm_wake.rtcWake(alarmTime.datetime)
-    print("init ")
+    print("init: found a new alarm")
